c067736e8b4c9f25b50b9dfaddc91f6d3e7e7103
abel mesfin <abelmesfin555@gmail.com>
2022-10-06 10:58:13 +0300

    inital commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * bubble_sort - sorts an array of intigers in ascending order.
+ * @array: array of intigers.
+ * @size: size of array.
+ *
+ * Return: always nothing.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp, i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+
+
+
