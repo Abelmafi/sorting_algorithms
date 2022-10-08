8a8a6fd73f5e9bca066b79d2376e31555ffa2e01
abel mesfin <abelmesfin555@gmail.com>
2022-10-08 13:22:22 +0300

    inital commit

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts the elements of array by exchange of far items
+ * in sorting order.
+ * @array: array to be sorted.
+ * @size: array size.
+ *
+ * Return: None.
+ */
+void shell_sort(int *array, size_t size)
+{
+	int interval = 0, gap, temp;
+	size_t i, j, g = 0;
+
+	while (g < size / 3)
+	{
+		g = (interval * 3) + 1;
+		interval++;
+	}
+	for (gap = g; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			array[j] = array[j - gap];
+		array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
