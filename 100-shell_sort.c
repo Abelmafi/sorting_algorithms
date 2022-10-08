81feb1d047d09c2d9429f8763c6c5175c7e42675
abel mesfin <abelmesfin555@gmail.com>
2022-10-08 13:30:57 +0300

    compile error fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,19 +9,17 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int interval = 0, temp;
-	size_t i, j, g = 0, gap;
+	int temp = 0;
+	size_t i, j, gap = 0;
 
-	while (g < size / 3)
-	{
-		g = (interval * 3) + 1;
-		interval++;
-	}
-	for (gap = g; gap > 0; gap = (gap - 1) / 3)
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
 		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
+		temp = array[i];
 		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			array[j] = array[j - gap];
 		array[j] = temp;
