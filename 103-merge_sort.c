3f6c95ead22810593d9bdab2af3fa41fb4a129a4
abel mesfin <abelmesfin555@gmail.com>
2022-10-10 16:39:09 +0300

    output error fix

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -4,78 +4,57 @@
 /**
  * merge_half - merge sorted array in sorted manner.
  * @array: array to be merged.
+ * @buff: temporary buffer array.
  * @l_start: array starting index.
  * @mid: array mid index.
  * @r_end: array ending index.
  *
  * return: None.
  */
-void merge_half(int *array, int l_start, int mid, int r_end)
+void merge_half(int *array, int *buff, int l_start, int mid, int r_end)
 {
-	int l_size = mid - l_start + 1;
-	int r_start = mid + 1;
-	int r_size = r_end - mid;
-	int *L, *R;
-	int i, j, index = l_start;
+	int r_start = mid;
+	int l_end = mid;
+	int r = r_start, l = l_start;
+	int i = 0, j;
 
-	L = malloc(sizeof(int) * l_size);
-	R = malloc(sizeof(int) * r_size);
-	for (i = 0; i < l_size; i++)
-		L[i] = array[l_start + i];
-	for (j = 0; j < r_size; j++)
-		R[j] = array[r_start + j];
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(L, i);
+	printf("Merging...\n[left]: ");
+	print_array(array + l_start, mid - l_start);
 	printf("[right]: ");
-	print_array(R, j);
-	i = 0;
-	j = 0;
-	while (i < l_size && j < r_size)
-	{
-		if (L[i] <= R[j])
-		{	array[index] = L[i];
-			i++;	}
-		else
-		{	array[index] = R[j];
-			j++;	}
-		index++;
-	}
-	while (i < l_size)
-	{	array[index] = L[i];
-		i++;
-		index++;	}
-	while (j < r_size)
-	{	array[index] = R[j];
-		j++;
-		index++;	}
-	free(L);
-	free(R);
+	print_array(array + r_start, r_end - mid);
+
+	while (l < l_end && r < r_end)
+		buff[i++] = (array[l] <= array[r]) ? array[l++] : array[r++];
+	while (l < l_end)
+		buff[i++] = array[l++];
+	while (r < r_end)
+		buff[i++] = array[r++];
+	for (j = l_start, i = 0; j < r_end; j++)
+		array[j] = buff[i++];
 	printf("[Done]: ");
-	print_array(array, index);
+	print_array(array + l_start, r_end - l_start);
 }
 /**
  * merge_sortt - sort a given array using merge soart algorithm.
  * @array: array to be sorted.
+ * @buff: temporary buffer array.
  * @left_start: array starting index.
  * @right_end: array last index.
- * @size: array size.
  *
  * Return: None.
  */
-void merge_sortt(int *array, int left_start, int right_end, int size)
+void merge_sortt(int *array, int *buff,  int left_start, int right_end)
 {
 	int middle;
 
-	if (left_start < right_end)
+	if (right_end - left_start > 1)
 	{
-		middle = (left_start + right_end) / 2;
+		middle = left_start + (right_end - left_start) / 2;
 
-		merge_sortt(array, left_start, middle, size);
-		merge_sortt(array, middle + 1, right_end, size);
+		merge_sortt(array, buff, left_start, middle);
+		merge_sortt(array, buff, middle, right_end);
 
-		merge_half(array, left_start, middle, right_end);
+		merge_half(array, buff, left_start, middle, right_end);
 	}
 }
 /**
@@ -88,6 +67,12 @@ void merge_sortt(int *array, int left_start, int right_end, int size)
  */
 void merge_sort(int *array, size_t size)
 {
-	merge_sortt(array, 0, size - 1, size);
-	print_array(array, size);
+	int *buff;
+
+	buff = malloc(sizeof(int) * size);
+	if (!buff)
+		return;
+
+	merge_sortt(array, buff,  0, size);
+	free(buff);
 }
